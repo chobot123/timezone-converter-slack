caefc00b1801f342f5bb82d9db13dc7f6ae06d36
Joshua <chojoshua99@gmail.com>
2023-11-27 16:02:04 -0800

    first commit

---
+++ src/main/java/com/chobot/timezonecoverter/timezoneconverterapp/TimezoneConverterAppApplication.java
@@ -0,0 +1,13 @@
+package com.chobot.timezonecoverter.timezoneconverterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezoneConverterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezoneConverterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chobot/timezonecoverter/timezoneconverterapp/TimezoneConverterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chobot.timezonecoverter.timezoneconverterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneConverterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
